f9293aa08272452e76b14fa183f04e3213861323
Do Vy <vydo383@gmail.com>
2023-07-31 10:57:32 +0700

    Add new feature: Sharing Activities

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
 import Dashboard from './views/dashboard/Dashboard'
 import AdminAccount from './views/accounts/admins/AdminAccounts'
+import authApi from './api/authApi'
 
 const loading = (
   <div className="pt-3 text-center">
@@ -24,30 +25,45 @@ const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 const App = () =>
-
   {
-    // Use state to track the user's login status
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track the user's login status
 
-  // Function to handle successful login
-  const handleLogin = () => {
+    // Function to handle successful login
+  const handleLoginSuccess = () => {
     // Perform your login logic here
     // For example, you can set a token or user information in local storage upon successful login
     // Then set isLoggedIn to true
     setIsLoggedIn(true);
   };
+  //    // Kiểm tra xem người dùng đã đăng nhập hay chưa
+  // const isLoggedIn = authApi.isLoggedIn();
+
+  // Nếu chưa đăng nhập, chuyển hướng đến trang đăng nhập
+  // if (!isLoggedIn) {
+  //   return <Redirect to="/login" />;
+  // }
+    // Use state to track the user's login status
+  //const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // if(!token) {
+  //   return <Login setToken={setToken} />
+  // }
+
+  //handleLogin: đc sd để cập nhật trạng thái đăng nhập của người dùng.
+  //Hàm handleLogin được truyền xuống file Login.js qua props để được gọi khi người dùng nhấn nút đăng nhập.
+
+  // Function to handle successful login
+  // const handleLoginSucess = () => {
+  //   // Perform your login logic here
+  //   // For example, you can set a token or user information in local storage upon successful login
+  //   // Then set isLoggedIn to true
+  //   setIsLoggedIn(true);
+  // };
     return (
       <BrowserRouter>
         <Suspense fallback={loading}>
           <Routes>
-            {/* <Route exact path="/login" name="Login Page" element={<Login onLoginSuccess={handleLogin}/>} />
-            <Route exact path="/register" name="Register Page" element={<Register />} />
-            <Route exact path="/404" name="Page 404" element={<Page404 />} />
-            <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route exact path="/" name="Login Page" element={<Login />} />
-            <Route path="*" element={<DefaultLayout />} /> */}
-
-            <Route exact path="/login" name="Login Page" element={<Login />} />
+            <Route exact path="/login" name="Login Page" element={<Login onLoginSuccess={handleLoginSuccess}/>} />
             <Route exact path="/register" name="Register Page" element={<Register />} />
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
@@ -58,15 +74,8 @@ const App = () =>
             <Route path="/*" element={<DefaultLayout />} />
           ) : (
             // Redirect to login page if not logged in
-            <Route path="*" element={<Login onLoginSuccess={handleLogin} />} />
+            <Route path="*" element={<Login onLoginSuccess={handleLoginSuccess}/>} />
           )}
-
-            {/* <Route exact path="/" name="Login Page" element={<Login />} />
-            <Route exact path="/register" name="Register Page" element={<Register />} />
-            <Route exact path="/404" name="Page 404" element={<Page404 />} />
-            <Route exact path="/500" name="Page 500" element={<Page500 />} /> */}
-            {/* <Route path="/deflautlayout" element={<DefaultLayout />} /> */}
-            {/* <Route path="/accounts/admins" element={<DefaultLayout><AdminAccount /></DefaultLayout>} /> */}
           </Routes>
         </Suspense>
       </BrowserRouter>
---
+++ src/api/authApi.js
@@ -0,0 +1,47 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
+import axiosClient from "./axiosClient";
+
+
+const authApi = {
+  login: async (email, password) => {
+    try {
+      const url = '/Login/Login';
+      const response = await axiosClient.post(url, { email, password });
+      console.log('Response:', response); // Log the full response to inspect its structure
+
+      // Get the token directly from the response
+       const token = response.token;
+      // const {content} = response;
+      // const {login, token} = content;
+      // const {id, fullName, urlAvatar, roles} = login;
+      const {id, fullName, urlAvatar, roles} = response.login;
+
+      //Xác thực và lưu trữ token sau khi người dùng đăng nhập thành công
+      localStorage.setItem('token', token);
+      //Lưu trữ thông tin người dùng trong local storage
+      localStorage.setItem('user', JSON.stringify({id, fullName, urlAvatar, roles}));
+      return token;
+    } catch (error) {
+      console.log('API Error:', error);
+      throw new Error('Invalid username or password. Please try again.');
+    }
+  },
+  logout: () => {
+    // Perform any logout logic here, e.g., clear local storage, remove tokens, etc.
+    localStorage.removeItem('token');
+    localStorage.removeItem('user'); // xóa thông tin người dùng khi logout
+  },
+  // You can add more authentication-related functions if needed, e.g., check if the user is authenticated, get the current user, etc.
+  isLoggedIn: () => {
+    const token = localStorage.getItem('token');
+    return !!token; // Nếu token tồn tại, hàm trả về true, ngược lại trả về false
+  },
+  getUserInfo: () => {
+    const userString = localStorage.getItem('user');
+    return userString ? JSON.parse(userString) : null; //parse thông tin người dùng từ local storage
+  }
+}
+
+export default authApi;
